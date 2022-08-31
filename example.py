542ed9f5d2dca95a357e4b64a11e50b1b91a63b8
Nakano Yoichi <112535284+nakanoyo@users.noreply.github.com>
2022-09-01 02:45:38 +0700

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 # An example of command use case
 
-from command import Program, command, run
+from command4py import Program, command, run
 
 
 class MyProgram(Program):
 	@command()
-	def echo():
-		pass
+	def echo(self, value: str, end = "\n"):
+		print(value, end="\n")
+
+run(MyProgram)
