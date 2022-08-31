71a2a629fcd93a9153636538df3e43aef48d72a1
Nakano Yoichi <112535284+nakanoyo@users.noreply.github.com>
2022-09-01 02:48:00 +0700

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
-# An example of command use case
-
 from command4py import Program, command, run
 
 
 class MyProgram(Program):
 	@command()
+	# value is a positional argument so you just need to pass it
+	# end is an optional argument an will be translated as --end or -e in the command line
 	def echo(self, value: str, end = "\n"):
-		print(value, end="\n")
+		print(value, end=end)
 
 run(MyProgram)
